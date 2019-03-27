5de55c25af0eb25467165b160b14fb613ae49626
luksari <luksari23@gmail.com>
2019-03-27 21:35:53 +0100

    Updates

---
+++ precache-manifest.c4ca23611b01e0d92ef00cce9e5a25eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b6090e6373e664846fcf1f76ee20dad",
+    "url": "/weather-app/static/media/ic_location.5b6090e6.svg"
+  },
+  {
+    "revision": "8d9c99a4f19116c6d931",
+    "url": "/weather-app/static/js/runtime~main.02d38d5d.js"
+  },
+  {
+    "revision": "eb5987b3c5b78352aac6",
+    "url": "/weather-app/static/js/main.2407fe38.chunk.js"
+  },
+  {
+    "revision": "e68717c8fcbae461e59b",
+    "url": "/weather-app/static/js/2.f31b80da.chunk.js"
+  },
+  {
+    "revision": "eb5987b3c5b78352aac6",
+    "url": "/weather-app/static/css/main.a2084ed7.chunk.css"
+  },
+  {
+    "revision": "9c18bb5f9d2767add95fc104036220c6",
+    "url": "/weather-app/index.html"
+  }
+];
